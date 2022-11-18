9aa82335e6bcb9cf4c451afc255c7c84aacf4ce1
jbrochu1 <111677770+jbrochu1@users.noreply.github.com>
2022-11-18 12:09:08 -0700

    styled product detail page

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -14,13 +14,14 @@ function ProductPage ( { item, cartItems, setCartItems } ) {
     return (
         <>
         <div>
-        <div className="text-center">
-            <p className="text-center text-3xl"> {item.name}</p>
-            <p className="">{item.description}</p>
-        </div>
-            <img alt="" className="mainImage" src={item.img}/>
-            {/* <div className="">{item.description}</div> */}
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={add}>{"Add to Cart"}</button>
+            <div className="flex flex-col justify-center items-center">
+                <p className="text-center text-3xl"> {item.name}</p>
+                <p className="">{item.description}</p>
+                <p className="">${item.price}</p>
+                <img alt="" className="mainImage" src={item.img}/>
+                <button className="bg-blue-500 w-1/4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={add}>{"Add to Cart"}</button>
+            </div>
+            
         </div>
         </>
     )
